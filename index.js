347e86c0034d3ac761292d621c494f936854c4bc
alaahosnii <alaahosnii1000@gmail.com>
2025-07-25 16:06:45 +0300

    Update CORS configuration to allow requests from Vercel deployment URL

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json({ limit: '20mb' }));
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', "https://media-project-nine.vercel.app"], credentials: true }));
 
 // Mount auth routes
 const authRouter = require('./routes/auth');
